2d0028cfb151c082a40cc73f1db864945198d457
xspacex999 <70760261+xspacex999@users.noreply.github.com>
2021-03-29 16:17:29 +0530

    add login function with jsonwebtoken

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-
+const userSchema = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 const userSchema = mongoose.Schema({
 
     name: {
@@ -30,6 +32,44 @@ const userSchema = mongoose.Schema({
         type: Number,
     }
 })
+
+
+userSchema.pre('save', function(next) {
+    var user = this;
+
+    if(user.isModified('password')) {
+
+           bcrypt.genSaltSync(saltRounds, function(err, salt) {
+        if(err) return next(err);
+
+        bcrypt.hash(user.password, salt, function(err, hash){
+            if(err) return next(err);
+            user.password = hash
+        })
+    })
+} else {
+    next ()
+}
+    });
+ 
+
+ userSchema.methods.comparePassword = function(plainPassword, cb){
+     bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+         if (err) return cb(err);
+         cb(null, isMatch)
+     })
+ }
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(),'secret')
+
+    user.token = token;
+    user.save(function (err, user){
+        if(err) return cb(err)
+        cb(null, user);
+    })
+}
 
 
 const User = mongoose.model('User', userSchema)
