91c9d5ad5b5ee7227c0c1a9054410a6b14d54757
xspacex999 <70760261+xspacex999@users.noreply.github.com>
2021-03-29 09:31:25 +0530

    move private information into dev.js

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const express = require('express');
-const app = express();
+
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://wot50withcode:helloreact123@cluster0.xqajh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const config = require('./config/key');
+const { User } = require('./models/user');
+const app = express();
+
+mongoose.connect(config.mongoURI,
  {useNewUrlParser: true}).then(() => console.log('DB connected'))
                         .catch(err => console.error(err));
 
-app.get('/',(req, res) => {
-  res.send('hello world')
-})
+                   
+ 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+ app.use(cookieParser());              
 
+app.get('/', function (req, res) {
+                        res.send('hello world and hi')
+                         })
+
+app.post('/api/users/register', (req, res) => {
+  const user = new User(req.body)
+
+  user.save((err, userData) => {
+    if(err) return res.json ({ success: false, err})
+     return res.status(200).json({
+       success: true
+     })
+})
+  })
 
 
 app.listen(5000);
